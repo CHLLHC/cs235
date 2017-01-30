3d97049c122f82e3b727c517b5cf2e6beb462dde
CHL-Lenovo-VM <chl41993@gmail.com>
2017-01-30 00:32:57 -0800

    Looks good.

--- UCR_time_series_test.m
+++ UCR_time_series_test.m
@@ -1,31 +1,46 @@
 function UCR_time_series_test %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% (C) Eamonn Keogh %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-TRAIN = load('synthetic_control_TRAIN'); % Only these two lines need to be changed to test a different dataset  %
-TEST  = load('synthetic_control_TEST' ); % Only these two lines need to be changed to test a different dataset  %
+TRAIN = load('leaf.csv'); % Only these two lines need to be changed to test a different dataset  %
+TEST  = load('PhalangesOutlinesCorrect_TEST' ); % Only these two lines need to be changed to test a different dataset  %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 
 TRAIN_class_labels = TRAIN(:,1);     % Pull out the class labels.
 TRAIN(:,1) = [];                     % Remove class labels from training set.
+TRAIN(:,1) = [];                     % Remove number.
+TRAIN = bsxfun(@minus,TRAIN, min(TRAIN));
+TRAIN = bsxfun(@rdivide,TRAIN, max(TRAIN));
+[Class,Count] = mode(TRAIN_class_labels);
+Default = 1-(Count/size(TRAIN_class_labels,1));
 TEST_class_labels = TEST(:,1);       % Pull out the class labels.
 TEST(:,1) = [];                      % Remove class labels from testing set.
 correct = 0; % Initialize the number we got correct
 
-for i = 1 : length(TEST_class_labels) % Loop over every instance in the test set
-   classify_this_object = TEST(i,:);
-   this_objects_actual_class = TEST_class_labels(i);
-   predicted_class = Classification_Algorithm(TRAIN,TRAIN_class_labels, classify_this_object);
+
+
+for i = 1 : length(TRAIN_class_labels) % Loop over every instance in the test set
+   classify_this_object = TRAIN(i,:);
+   this_objects_actual_class = TRAIN_class_labels(i);
+   %MinusThis = TRAIN;
+   %MinusThis(i,:) = [];
+   %MinusThis_class_labels = TRAIN_class_labels;
+   %MinusThis_class_labels(i,:) = [];
+   %predicted_class = Classification_Algorithm(MinusThis,MinusThis_class_labels, classify_this_object);
+   predicted_class = Classification_Algorithm(TRAIN,TRAIN_class_labels, classify_this_object, i);
    if predicted_class == this_objects_actual_class
        correct = correct + 1;
    end;
-   disp([int2str(i), ' out of ', int2str(length(TEST_class_labels)), ' done']) % Report progress
+   if (mod(i,10)==0)
+    disp([int2str(i), ' out of ', int2str(length(TRAIN_class_labels)), ' done']) % Report progress
+   end;
 end;
 
 %%%%%%%%%%%%%%%%% Create Report %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 disp(['The dataset you tested has ', int2str(length(unique(TRAIN_class_labels))), ' classes'])
 disp(['The training set is of size ', int2str(size(TRAIN,1)),', and the test set is of size ',int2str(size(TEST,1)),'.'])
 disp(['The time series are of length ', int2str(size(TRAIN,2))])
-disp(['The error rate was ',num2str((length(TEST_class_labels)-correct )/length(TEST_class_labels))])
+disp(['The default rate was ', num2str(Default)])
+disp(['The error rate was ',num2str((length(TRAIN_class_labels)-correct )/length(TRAIN_class_labels))])
 %%%%%%%%%%%%%%%%% End Report %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -33,14 +48,16 @@ disp(['The error rate was ',num2str((length(TEST_class_labels)-correct )/length(
 % neighbor using the Euclidean distance.
 % If you are advocating a new distance measure you just need to change the line marked "Euclidean distance"
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function predicted_class = Classification_Algorithm(TRAIN,TRAIN_class_labels,unknown_object)
+function predicted_class = Classification_Algorithm(TRAIN,TRAIN_class_labels,unknown_object, excludeIndex)
 best_so_far = inf;
  
  for i = 1 : length(TRAIN_class_labels)
-     compare_to_this_object = TRAIN(i,:);            
-       distance = sqrt(sum((compare_to_this_object - unknown_object).^2)); % Euclidean distance
-     if distance < best_so_far
+     if (i ~= excludeIndex)
+        compare_to_this_object = TRAIN(i,:);            
+        distance = sqrt(sum((compare_to_this_object - unknown_object).^2)); % Euclidean distance
+        if distance < best_so_far
           predicted_class = TRAIN_class_labels(i);
-     best_so_far = distance;
-    end
+         best_so_far = distance;
+        end
+     end
 end;
